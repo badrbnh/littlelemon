1b1586328299cd08ea9d78681cba656bdd88613a
badrbnh <badrechahide@gmail.com>
2024-02-10 15:44:05 +0100

    added viewa

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework import generics, viewsets, permissions
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer, UserSerializer
+from django.contrib.auth.models import User
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] 
